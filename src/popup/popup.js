4139bfb62c1c9513c36980561b882c5a349ec9a0
Paul Bichler <Paulbichler@hotmail.de>
2023-03-08 18:12:56 +0100

    Added URL input field with Add button
    - Added an input field to input new URLs and an add button to add the URL to the blocked list. At the moment, the add button only outputs the inputed text to the console.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,5 @@
 var focusModeToggle = document.getElementById("focusModeToggle");
-var urlList = document.getElementById("blockedUrlList");
+var urlListParentNode = document.getElementById("blockedUrlList");
 var blockedUrls = [];
 
 focusModeToggle.onclick = function() {
@@ -15,19 +15,42 @@ chrome.storage.local.get(["IsFocusModeOn"]).then((result) => {
 });
 
 chrome.storage.local.get(["BlockedUrls"]).then((result) => {
-    blockedUrls = result.BlockedUrls;
+    initBlockedUrls(result.BlockedUrls);
+});
+
+function initBlockedUrls(urls) {
+    blockedUrls = urls;
     
     for (let i = 0; i < blockedUrls.length; i++) {
-        const url = blockedUrls[i];
+        addUrlToHtmlList(blockedUrls[i], i);
+    }
 
-        urlList.innerHTML +=
-        `<li class="py-3 sm:py-4" id="blockedUrl-` + i +`">
+    let addUrlForm = document.getElementById("addUrlForm");
+    addUrlForm.submitButton.onclick = function() {
+        console.log(addUrlForm.inputBox.value);
+    };
+}
+
+function addUrlToBlockedList(url) {
+    addUrlToHtmlList(url, blockedUrls.push(url) - 1);
+    chrome.storage.local.set({ BlockedUrls: blockedUrls });
+}
+
+function removeFromBlockedListAt(index) {
+    blockedUrls.splice(index, 1);
+    chrome.storage.local.set({ BlockedUrls: blockedUrls });
+    urlListParentNode.removeChild(document.getElementById("blockedUrl-" + index));
+}
+
+function addUrlToHtmlList(url, indexInArr) {
+    urlListParentNode.innerHTML +=
+        `<li class="py-3 sm:py-4" id="blockedUrl-` + indexInArr +`">
             <div class="flex items-center space-x-4">
                 <div class="flex-1 items-center min-w-0">
                     <p class="text-base text-gray-500 dark:text-gray-400">` + url +`</p>
                 </div>
                 <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+                    <button type="button" id="urlRemoveButton-` + indexInArr + `" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Remove URL</span>
                     </button>
@@ -35,16 +58,5 @@ chrome.storage.local.get(["BlockedUrls"]).then((result) => {
             </div>
         </li>`;
 
-        document.getElementById("blockedUrl-" + i).onclick = function() {removeFromBlockedListAt(i);};
-    }
-});
-
-function addUrlToBlockedList(url) {
-    blockedUrls.push(url);
-}
-
-function removeFromBlockedListAt(index) {
-    blockedUrls.splice(index, 1);
-    chrome.storage.local.set({ BlockedUrls: blockedUrls });
-    urlList.removeChild(document.getElementById("blockedUrl-" + index));
+        document.getElementById("urlRemoveButton-" + indexInArr).onclick = function() { removeFromBlockedListAt(indexInArr); };
 }
